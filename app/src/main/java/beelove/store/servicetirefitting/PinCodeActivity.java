bdce2564aaf069d866edfd8f97068ac51a6f90ff
Stempil <stempil90@gmail.com>
2022-11-10 21:47:25 +0500

    UI_login_screen+change_pin_screen

---
+++ app/src/main/java/beelove/store/servicetirefitting/PinCodeActivity.java
@@ -0,0 +1,14 @@
+package beelove.store.servicetirefitting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_code);
+    }
+}
