eb22abad6e1a2ef24ee3a2c96ece93ef91ec3fd3
Stempil <stempil90@gmail.com>
2022-11-15 20:30:42 +0500

    add logic to PinCodeActivity.java change Phone to Login Screen

--- app/src/main/java/beelove/store/servicetirefitting/MainActivity.java
+++ app/src/main/java/beelove/store/servicetirefitting/MainActivity.java
@@ -14,12 +14,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
         private EditText editText;
         private Spinner spinner;
         private Button button;
+        private String phone;
 
 
         @Override
@@ -31,84 +35,27 @@ public class MainActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinnerCity);
         button = findViewById(R.id.btnContinue);
 
-                editText.addTextChangedListener(new CurrencyTextWatcher());
+
+
+        editText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+
 
         }
+        /*--Тут должен быт метод сохранения телефона если он нам куда-то нужен--*/
+       /* @Override
+        protected void onSaveInstanceState(Bundle outState) {
+                outState.putString("Phone", phone);
+                if(phone!=null)
 
-        private class CurrencyTextWatcher implements TextWatcher {  /// Зачем объявлен новый класс?
+                super.onSaveInstanceState(outState);
+        }*/
 
-                StringBuilder sb = new StringBuilder();
-                boolean ignore;
-
-                private final char numPlace = 'X';
-
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {
-                        if (!ignore) {
-                                removeFormat(editable.toString());
-                                applyFormat(sb.toString());
-                                ignore = true;
-                                editable.replace(0, editable.length(), sb.toString());
-                                ignore = false;
-                        }
-                }
-
-                private void removeFormat(String text) {
-                        sb.setLength(0);
-                        for (int i = 0; i < text.length(); i++) {
-                                char c = text.charAt(i);
-                                if (isNumberChar(c)) {
-                                        sb.append(c);
-                                }
-                        }
-                }
-
-                private void applyFormat(String text) {
-                        String template = getTemplate(text);
-                        sb.setLength(0);
-                        for (int i = 0, textIndex = 0; i < template.length() && textIndex < text.length(); i++) {
-                                if (template.charAt(i) == numPlace) {
-                                        sb.append(text.charAt(textIndex));
-                                        textIndex++;
-                                } else {
-                                        sb.append(template.charAt(i));
-                                }
-                        }
-                }
-
-                private boolean isNumberChar(char c) {
-                        return c >= '0' && c <= '9';
-                }
-
-                private String getTemplate(String text) {
-                        if (text.startsWith("8")) {
-                                return "X (XXX) XXX-XX-XX";
-                        }
-                        if (text.startsWith("7")) {
-                                return "+X (XXX) XXX-XX-XX";
-                        }
-                        if (text.startsWith("9")) {
-                                return "+7 (XXX) XXX-XX-XX";
-                        }
-                        return  "+X (XXX) XXX-XX-XX";
-                }
-        }
         public void clickBtnConinue(View view) {
+
                 Intent intent = new Intent(this, PinCodeActivity.class);
                 startActivity(intent);
         }
 
-        public void clickInputPhone(View view) {
 
-        }
 }
--- app/src/main/java/beelove/store/servicetirefitting/PinCodeActivity.java
+++ app/src/main/java/beelove/store/servicetirefitting/PinCodeActivity.java
@@ -4,47 +4,112 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PinCodeActivity extends AppCompatActivity {
+    private int counterCircles = 0;
+
+    private TextView circleOne;
+    private TextView circleTwo;
+    private TextView circleThree;
+    private TextView circleFour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_code);
+        circleOne = findViewById(R.id.circle_1);
+        circleTwo = findViewById(R.id.circle_2);
+        circleThree = findViewById(R.id.circle_3);
+        circleFour = findViewById(R.id.circle_4);
     }
+
+
+
+    public void changeCircle() {
+
+        if (counterCircles == 0 || counterCircles < 4) {
+            counterCircles++;
+            switch (counterCircles) {
+                case (1):
+                    circleOne.setBackgroundResource(R.drawable.circle_full);
+                    break;
+                case (2):
+                    circleTwo.setBackgroundResource(R.drawable.circle_full);
+                    break;
+                case (3):
+                    circleThree.setBackgroundResource(R.drawable.circle_full);
+                    break;
+                case (4):
+                    circleFour.setBackgroundResource(R.drawable.circle_full);
+                    break;
+            }
+
+        }
+    }
+
+    public void removeCircle() {
+        if (counterCircles > 0 || counterCircles < 5) {
+
+            switch (counterCircles) {
+                case (1):
+                    circleOne.setBackgroundResource(R.drawable.circle_empty);
+                    break;
+                case (2):
+                    circleTwo.setBackgroundResource(R.drawable.circle_empty);
+                    break;
+                case (3):
+                    circleThree.setBackgroundResource(R.drawable.circle_empty);
+                    break;
+                case (4):
+                    circleFour.setBackgroundResource(R.drawable.circle_empty);
+                    break;
+            }
+            counterCircles--;
+        }
+    }
+
 
     public void clickBtnOne(View view) {
+        changeCircle();
+
+        }
+
+
+    public void clickBtnTwo(View view) {changeCircle();
     }
 
-    public void clickBtnTwo(View view) {
+    public void clickBtnThree(View view) {changeCircle();
     }
 
-    public void clickBtnThree(View view) {
+    public void clickBtnFour(View view) {changeCircle();
     }
 
-    public void clickBtnFour(View view) {
+    public void clickBtnFive(View view) {changeCircle();
     }
 
-    public void clickBtnFive(View view) {
+    public void clickBtnSix(View view) {changeCircle();
     }
 
-    public void clickBtnSix(View view) {
+    public void clickBtnSeven(View view) {changeCircle();
     }
 
-    public void clickBtnSeven(View view) {
+    public void clickBtnEight(View view) {changeCircle();
     }
 
-    public void clickBtnEight(View view) {
+    public void clickBtnNine(View view) {changeCircle();
     }
-
-    public void clickBtnNine(View view) {
-    }
-    public void clickBtnZero(View view) {
+    public void clickBtnZero(View view) {changeCircle();
     }
 
     public void clickBtnFinger(View view) {
     }
 
     public void clickBtnBackspace(View view) {
-    }
+            removeCircle();
+        }
+
 }
